cce9b654b1a74d48fa60f1e30e40bf8b4fceb757
Stefan Larsson <stefan@lastsys.com>
2018-11-19 15:34:07 +0100

    Corrected calculations.

--- cmd/c64image.go
+++ cmd/c64image.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/lastsys/c64image/internal/c64image"
+	"image"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -13,6 +14,11 @@ func main() {
 		panic(err)
 	}
 
+	rgbChannel := make(chan *image.RGBA)
+	cie76Channel := make(chan *image.RGBA)
+	cie94Channel := make(chan *image.RGBA)
+	cie2000Channel := make(chan *image.RGBA)
+
 	for _, f := range files {
 
 		if strings.HasSuffix(f.Name(), ".jpg") {
@@ -20,11 +26,20 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			baseFilename := strings.Trim(f.Name(), ".jpg")
+			baseFilename := strings.TrimSuffix(f.Name(), ".jpg")
 			log.Printf("Processing %v\n", baseFilename)
-			c64Image := c64image.ConvertImage(originalImage)
-			targetFile := "c64_" + baseFilename + ".png"
-			c64image.SaveImage(c64Image, targetFile)
+			go c64image.ConvertImage(originalImage, c64image.RGBMethod, rgbChannel)
+			go c64image.ConvertImage(originalImage, c64image.CIE76, cie76Channel)
+			go c64image.ConvertImage(originalImage, c64image.CIE94, cie94Channel)
+			go c64image.ConvertImage(originalImage, c64image.CIE2000, cie2000Channel)
+
+			log.Print("Saving\n")
+			c64image.SaveImage(<-rgbChannel, "c64_"+baseFilename+"_RGB.png")
+			c64image.SaveImage(<-cie76Channel, "c64_"+baseFilename+"_CIE76.png")
+			c64image.SaveImage(<-cie94Channel, "c64_"+baseFilename+"_CIE94.png")
+			c64image.SaveImage(<-cie2000Channel, "c64_"+baseFilename+"_CIE2000.png")
+			log.Print("Done.\n")
+			log.Print("-----------------------------\n")
 		}
 	}
 }
--- internal/c64image/converter.go
+++ internal/c64image/converter.go
@@ -45,9 +45,29 @@ type xyz struct {
 	z float64
 }
 
+type rgb struct {
+	r float64
+	g float64
+	b float64
+}
+
 const (
 	C64Width  = 320
 	C64Height = 200
+)
+
+type Method int
+
+const (
+	RGBMethod Method = iota
+	CIE76
+	CIE94
+	CIE2000
+)
+
+const (
+	deg2rad = math.Pi / 360.
+	rad2deg = 360. / math.Pi
 )
 
 func LoadImage(filename string) (*image.RGBA, error) {
@@ -78,7 +98,7 @@ func SaveImage(img *image.RGBA, filename string) error {
 	return nil
 }
 
-func ConvertImage(img *image.RGBA) *image.RGBA {
+func ConvertImage(img *image.RGBA, method Method, returnChannel chan *image.RGBA) {
 	aspectRatio := float64(img.Rect.Size().X) / float64(img.Rect.Size().Y)
 
 	targetWidth := 320
@@ -91,44 +111,72 @@ func ConvertImage(img *image.RGBA) *image.RGBA {
 
 	for j := 0; j < targetHeight; j++ {
 		for i := 0; i <= targetWidth; i++ {
-			colorEstimate := meanBlockColor(img,
+			colorEstimate, rgbEstimate := meanBlockColor(img,
 				image.Rect(i*blockWidth, j*blockHeight, (i+1)*blockWidth-1, (j+1)*blockHeight-1))
-			ci := closestC64Color(colorEstimate)
+			ci := closestC64Color(colorEstimate, rgbEstimate, method)
 			targetImage.SetRGBA(i*2, j, C64Colors[ci])
 			targetImage.SetRGBA(i*2+1, j, C64Colors[ci])
 		}
 	}
 
-	return targetImage
+	returnChannel <- targetImage
 }
 
 // Calculate mean color of image block.
-func meanBlockColor(img *image.RGBA, rect image.Rectangle) cielab {
+func meanBlockColor(img *image.RGBA, rect image.Rectangle) (cielab, color.RGBA) {
 	avglab := cielab{0, 0, 0}
+	avgrgb := rgb{0, 0, 0}
+	var rgbColor color.RGBA
 	for x := rect.Min.X; x < rect.Max.X; x++ {
 		for y := rect.Min.Y; y < rect.Max.Y; y++ {
-			lab := convertRGBAtoCIELAB(img.RGBAAt(x, y))
+			rgbColor = img.RGBAAt(x, y)
+			lab := convertRGBAtoCIELAB(rgbColor)
 			avglab.l += lab.l
 			avglab.a += lab.a
 			avglab.b += lab.b
+
+			avgrgb.r += float64(rgbColor.R)
+			avgrgb.g += float64(rgbColor.G)
+			avgrgb.b += float64(rgbColor.B)
 		}
 	}
 	pixelCount := float64((rect.Max.X - rect.Min.X) * (rect.Max.Y - rect.Min.Y))
+
 	avglab.l /= pixelCount
 	avglab.a /= pixelCount
 	avglab.b /= pixelCount
-	return avglab
+
+	avgrgb.r /= pixelCount
+	avgrgb.g /= pixelCount
+	avgrgb.b /= pixelCount
+
+	avgrgbColor := color.RGBA{uint8(avgrgb.r), uint8(avgrgb.g), uint8(avgrgb.b), 255}
+
+	return avglab, avgrgbColor
 }
 
 // Find index of closest color in C64Colors.
-func closestC64Color(color cielab) int {
+func closestC64Color(color cielab, rgbColor color.RGBA, method Method) int {
 	bestIndex := 0
 	bestDistance := math.Inf(1)
 	for i, c64Color := range C64Colors {
 		lab2 := convertRGBAtoCIELAB(c64Color)
-		if distance := cie94distance(color, lab2); distance < bestDistance {
+
+		var deltaE float64
+		switch method {
+		case RGBMethod:
+			deltaE = rgbDistance(rgbColor, c64Color)
+		case CIE76:
+			deltaE = cie76distance(color, lab2)
+		case CIE94:
+			deltaE = cie94distance(color, lab2)
+		case CIE2000:
+			deltaE = cie2000distance(color, lab2)
+		}
+
+		if deltaE < bestDistance {
 			bestIndex = i
-			bestDistance = distance
+			bestDistance = deltaE
 		}
 	}
 	return bestIndex
@@ -141,10 +189,36 @@ func rgbDistance(color1 color.RGBA, color2 color.RGBA) float64 {
 }
 
 func convertRGBAtoXYZ(rgba color.RGBA) xyz {
+	r := float64(rgba.R) / 255.0
+	g := float64(rgba.G) / 255.0
+	b := float64(rgba.B) / 255.0
+
+	if r > 0.04045 {
+		r = math.Pow((r+0.055)/1.055, 2.4)
+	} else {
+		r = r / 12.92
+	}
+
+	if g > 0.04045 {
+		g = math.Pow((g+0.055)/1.055, 2.4)
+	} else {
+		g = g / 12.92
+	}
+
+	if b > 0.04045 {
+		b = math.Pow((b+0.055)/1.055, 2.4)
+	} else {
+		b = b / 12.92
+	}
+
+	r *= 100.0
+	g *= 100.0
+	b *= 100.0
+
 	return xyz{
-		x: 0.412453*float64(rgba.R) + 0.357580*float64(rgba.G) + 0.180423*float64(rgba.B),
-		y: 0.212671*float64(rgba.R) + 0.715160*float64(rgba.G) + 0.072169*float64(rgba.B),
-		z: 0.019334*float64(rgba.R) + 0.119193*float64(rgba.G) + 0.950227*float64(rgba.B),
+		x: 0.4124564*r + 0.3575761*g + 0.1804375*b,
+		y: 0.2126729*r + 0.7151522*g + 0.0721750*b,
+		z: 0.0193339*r + 0.1191920*g + 0.9503041*b,
 	}
 }
 
@@ -155,24 +229,17 @@ func convertRGBAtoCIELAB(rgba color.RGBA) cielab {
 	Yn := 100.0
 	Zn := 108.883
 
-	lf := func(t float64) float64 {
-		if t > 0.008856 {
-			return 116.0*math.Pow(t, 1.0/3.0) - 16.0
-		}
-		return 903.3 * t
-	}
-
 	f := func(t float64) float64 {
-		if t > 0.008856 {
-			return math.Pow(t, 1.0/3.0)
+		if t > math.Pow(24./116., 3.) {
+			return math.Pow(t, 1./3.)
 		}
-		return 7.787*t + 16.0/116.0
+		return (841./108.)*t + 16./116.
 	}
 
 	return cielab{
-		l: lf(xyz.y / Yn),
-		a: 500.0 * (f(xyz.x/Xn) - f(xyz.y/Yn)),
-		b: 200.0 * (f(xyz.y/Yn) - f(xyz.z/Zn)),
+		l: 116.*f(xyz.y/Yn) - 16.,
+		a: 500. * (f(xyz.x/Xn) - f(xyz.y/Yn)),
+		b: 200. * (f(xyz.y/Yn) - f(xyz.z/Zn)),
 	}
 }
 
@@ -181,20 +248,110 @@ func cie76distance(c1 cielab, c2 cielab) float64 {
 }
 
 func cie94distance(col1 cielab, col2 cielab) float64 {
-	dL := col1.l - col2.l
-	c1 := math.Sqrt(col1.a*col1.a + col1.b*col1.b)
-	c2 := math.Sqrt(col2.a*col2.a + col2.b*col2.b)
-	da := col1.a - col2.a
-	db := col2.b - col2.b
-	dCab := c1 - c2
-	dHab := math.Sqrt(da*da + db*db + dCab*dCab)
-	kL := 1.0
-	kC := 1.0
-	kH := 1.0
-	k1 := 0.045
-	k2 := 0.015
-	sL := 1.0
-	sC := 1.0 + k1*c1
-	sH := 1.0 + k2*c1
-	return math.Pow(dL/(kL*sL), 2) + math.Pow(dCab/(kC*sC), 2) + math.Pow(dHab/(kH*sH), 2)
+	xC1 := math.Sqrt(col1.a*col1.a + col1.b*col1.b)
+	xC2 := math.Sqrt(col2.a*col2.a + col2.b*col2.b)
+	xDL := col2.l - col1.l
+	xDC := xC2 - xC1
+	xDE := math.Sqrt(math.Pow(col1.l-col2.l, 2.) +
+		math.Pow(col1.a-col2.a, 2.) +
+		math.Pow(col1.b-col2.b, 2.))
+	xDH := xDE*xDE - xDL*xDL - xDC*xDC
+	if xDH > 0. {
+		xDH = math.Sqrt(xDH)
+	} else {
+		xDH = 0.
+	}
+	xSC := 1. + (0.045 * xC1)
+	xSH := 1. + (0.015 * xC1)
+
+	xDL /= 1.
+	xDC /= 1. * xSC
+	xDH /= 1. * xSH
+
+	return xDL*xDL + xDC*xDC + xDH*xDH
+}
+
+func cielab2hue(a, b float64) float64 {
+	var bias = 0.0
+	if a >= 0. && math.Abs(b) < 1e-6 {
+		return 0.
+	} else if a < 0. && math.Abs(b) < 1e-6 {
+		return 180.
+	} else if math.Abs(a) < 1e-6 && b > 0. {
+		return 90.
+	} else if math.Abs(a) < 1e-6 && b < 0. {
+		return 270.
+	} else if a > 0. && b > 0. {
+		bias = 0.
+	} else if a < 0. {
+		bias = 180.
+	} else if a > 0. && b < 0. {
+		bias = 360.
+	}
+
+	return rad2deg*math.Atan(b/a) + bias
+}
+
+func cie2000distance(col1 cielab, col2 cielab) float64 {
+	xC1 := math.Sqrt(col1.a*col1.a + col1.b*col1.b)
+	xC2 := math.Sqrt(col2.a*col2.a + col2.b*col2.b)
+	xCX := (xC1 + xC2) / 2.
+	xGX := .5 * (1. - math.Sqrt(math.Pow(xCX, 7.)/(math.Pow(xCX, 7.)+math.Pow(25., 7.))))
+	xNN := (1. + xGX) * col1.a
+	xC1 = math.Sqrt(xNN*xNN + col1.b*col1.b)
+	xH1 := cielab2hue(xNN, col1.b)
+	xNN = (1. + xGX) * col2.a
+	xC2 = math.Sqrt(xNN*xNN + col2.b*col2.b)
+	xH2 := cielab2hue(xNN, col2.b)
+	xDL := col2.l - col1.l
+	xDC := xC2 - xC1
+	var xDH float64
+	if math.Abs(xC1*xC2) < 1e-6 {
+		xDH = 0
+	} else {
+		xNN = math.Round(xH2 - xH1)
+		if math.Abs(xNN) <= 180. {
+			xDH = xH2 - xH1
+		} else {
+			if xNN > 180. {
+				xDH = xH2 - xH1 - 360.
+			} else {
+				xDH = xH2 - xH1 + 360.
+			}
+		}
+	}
+	xDH = 2. * math.Sqrt(xC1*xC1) * math.Sin(deg2rad*(xDH/2.))
+	xLX := (col1.l + col2.l) / 2.
+	xCY := (xC1 + xC2) / 2.
+	var xHX float64
+	if math.Abs(xC1*xC2) < 1e-6 {
+		xHX = xH1 + xH2
+	} else {
+		xNN = math.Abs(math.Round(xH1 - xH2))
+		if xNN > 180. {
+			if (xH2 + xH1) < 360. {
+				xHX = xH1 + xH2 + 360.
+			} else {
+				xHX = xH1 + xH2 - 360.
+			}
+		} else {
+			xHX = xH1 + xH2
+		}
+		xHX /= 2
+	}
+	xTX := 1. - 0.17*math.Cos(deg2rad*(xHX-30.)) +
+		0.24*math.Cos(deg2rad*(2.*xHX)) +
+		0.32*math.Cos(deg2rad*(3.*xHX+6.)) -
+		0.20*math.Cos(deg2rad*(4.*xHX-63.))
+	xPH := 30. * math.Exp(-((xHX-275.)/25.)*((xHX-275.)/25.))
+	xRC := 2. * math.Sqrt(math.Pow(xCY, 7.)/(math.Pow(xCY, 7.)+math.Pow(25., 7.)))
+	xSL := 1. + (0.015*(xLX-50.)*(xLX-50.))/math.Sqrt(20.+(xLX-50.)*(xLX-50.))
+	xSC := 1. + 0.045*xCY
+	xSH := 1. + 0.015*xCY*xTX
+	xRT := -math.Sin(deg2rad*2.*xPH) * xRC
+	xDL /= xSL
+	xDC /= xSC
+	xDH /= xSH
+
+	return xDL*xDL + xDC*xDC + xDH*xDH + xRT*xDC*xDH
 }
